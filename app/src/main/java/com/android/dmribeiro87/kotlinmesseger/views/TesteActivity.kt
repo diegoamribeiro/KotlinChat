6374bc6c520f8ad1cb9b4aa2d95da35753ee24c2
Diego Ribeiro <dmrbeiro87@gmail.com>
2019-06-02 18:25:19 -0300

    Teste2

---
+++ app/src/main/java/com/android/dmribeiro87/kotlinmesseger/views/TesteActivity.kt
@@ -0,0 +1,13 @@
+package com.android.dmribeiro87.kotlinmesseger.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.dmribeiro87.kotlinmesseger.R
+
+class TesteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste)
+    }
+}
